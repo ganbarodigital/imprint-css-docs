061e4ca50936a3c97d9800473edcd20eb09e6200
Stuart Herbert <stuart@stuartherbert.com>
2024-06-29 22:34:18 +0100

    New: added ExternalLink component

--- imprint.config.ts
+++ imprint.config.ts
@@ -9,4 +9,9 @@ export default {
         'Style Mods',
         'Colors',
     ] as const,
+    externalLinks: {
+        "Astro": "https://astro.build",
+        "BBC Gel Typography": "https://www.bbc.co.uk/gel/features/typography",
+        "Mozilla MDN": "https://developer.mozilla.org/",
+    }
 }
