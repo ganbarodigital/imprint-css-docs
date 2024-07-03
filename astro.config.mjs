8ad1c26b3abe1a4d68f2b876670bc2993adda34c
Stuart Herbert <stuart@stuartherbert.com>
2024-07-03 20:03:01 +0100

    WIP: initial mobile navigation support

--- astro.config.mjs
+++ astro.config.mjs
@@ -33,4 +33,7 @@ export default defineConfig({
       applyBaseStyles: false,
     }),
   ],
+  server: {
+    host: true,
+  }
 });
--- imprint.config.ts
+++ imprint.config.ts
@@ -13,5 +13,6 @@ export default {
         "Astro": "https://astro.build",
         "BBC Gel Typography": "https://www.bbc.co.uk/gel/features/typography",
         "Mozilla MDN": "https://developer.mozilla.org/",
+        "Mozilla MDN Browser Detection": "https://developer.mozilla.org/en-US/docs/Web/HTTP/Browser_detection_using_the_user_agent",
     }
 }
